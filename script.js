da75634ee690eadb06af9db12012eb36bd596686
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-15 21:08:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function shortcut(s1, s2) {
   // your code here
+	if(s1.charAt(0)=="" || s2.charAt(0)==""){
+      return "";
+  }
+  let ans = s1.charAt(0)+s2.charAt(0);
+  return ans;
 }
 
 // Do not change the code below.
